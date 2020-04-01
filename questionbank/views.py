10d8883e8b36871430f3f346f46afb45da45c7fb
amal-san <amaldemaniax@gmail.com>
2020-04-01 18:27:39 +0530

    appropriateviews

--- questionbank/views.py
+++ questionbank/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import *
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, View, DetailView 
+
 # Create your views here.
 
 class QuestionListView(View):
@@ -16,7 +18,34 @@ class QuestionListView(View):
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+	template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        return {'name': 'reinout'}
+	def get_context_data(self, **kwargs):
+		subjects = Subject.objects.all()
+		return { 'subjects': subjects }
+
+class SubjectDetailView(TemplateView):
+
+	template_name = 'chapter.html'
+
+	def get_context_data(self, **kwargs):
+		chapters = Subject.objects.all().filter(id=self.kwargs['pk']).select_related()
+		return { 'chapters': chapters }
+
+class ChapterDetailView(TemplateView):
+
+	template_name = 'questions.html'
+
+	def get_context_data(self, **kwargs):
+		questions = Chapter.objects.all().filter(id=self.kwargs['pk']).select_related()
+		print(questions)
+		return { 'questions': questions }
+
+class QuestionDetailView(TemplateView):
+
+	template_name = 'question.html'
+
+	def get_context_data(self, **kwargs):
+		question = Question.objects.all().filter(id=self.kwargs['pk'])
+		print(question)
+		return { 'question': question }
