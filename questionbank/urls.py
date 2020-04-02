882b4fbf6fb0752b15ff5199d590cce769d1819c
amal-san <amaldemaniax@gmail.com>
2020-04-02 18:30:57 +0530

    login and logout

--- questionbank/urls.py
+++ questionbank/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('home/',IndexView.as_view(),name='index'),
     path('subject/<int:pk>/',SubjectDetailView.as_view(),name='subject_detail'),
     path('subject/chapter/<int:pk>/',ChapterDetailView.as_view(),name='chapter_detail'),
-    path('subject/chapter/question/<int:pk>/',QuestionDetailView.as_view(),name='question_detail')
+    path('subject/chapter/question/<int:pk>/',QuestionDetailView.as_view(),name='question_detail'),
+    path('access/',LoginView.as_view(),name='auth'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    
 ]
