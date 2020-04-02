641d2d394403ea0c3375434bc1a57f7a808122e0
amal-san <amaldemaniax@gmail.com>
2020-04-02 21:20:23 +0530

    modified

--- questionbank/urls.py
+++ questionbank/urls.py
@@ -5,10 +5,10 @@ from .views import *
 urlpatterns = [
     path('list/',QuestionListView.as_view() ),
     path('home/',IndexView.as_view(),name='index'),
-    path('subject/<int:pk>/',SubjectDetailView.as_view(),name='subject_detail'),
-    path('subject/chapter/<int:pk>/',ChapterDetailView.as_view(),name='chapter_detail'),
-    path('subject/chapter/question/<int:pk>/',QuestionDetailView.as_view(),name='question_detail'),
+    path('subject/<int:pk>',SubjectDetailView.as_view(),name='subject_detail'),
+    path('chapter/<int:pk>/',ChapterDetailView.as_view(),name='chapter_detail'),
+    path('question/<int:pk>/',QuestionDetailView.as_view(),name='question_detail'),
     path('access/',LoginView.as_view(),name='auth'),
     path('logout/',LogoutView.as_view(),name='logout'),
-    
+
 ]
