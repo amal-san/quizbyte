54cca48bcd82d408ae36a5a49a38cecd98e6219b
amal-san <amaldemaniax@gmail.com>
2020-04-03 14:33:33 +0530

    root_url_change

--- questionbank/urls.py
+++ questionbank/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('list/',QuestionListView.as_view() ),
-    path('home/',IndexView.as_view(),name='index'),
+    path('',IndexView.as_view(),name='index'),
     path('subject/<int:pk>',SubjectDetailView.as_view(),name='subject_detail'),
     path('chapter/<int:pk>/',ChapterDetailView.as_view(),name='chapter_detail'),
     path('question/<int:pk>/',QuestionDetailView.as_view(),name='question_detail'),
