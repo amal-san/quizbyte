b7dd87ed34651e05c72f8696a0eba772294c6fbe
amal-san <amaldemaniax@gmail.com>
2020-04-04 13:35:57 +0530

    single

--- quizbyte/wsgi.py
+++ quizbyte/wsgi.py
@@ -16,5 +16,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizbyte.settings')
 application = get_wsgi_application()
 
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
